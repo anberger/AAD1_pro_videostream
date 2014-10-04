32542a55f3b17c2d3bb8b63ef2f39abbe72c5965
anberger <bergerandy@hotmail.com>
2014-10-04 18:56:39 +0200

    initial commit

---
+++ AAD1_pro_videostream/src/com/aad1/aad1_pro_videostream/MainActivity.java
@@ -0,0 +1,13 @@
+package com.aad1.aad1_pro_videostream;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
